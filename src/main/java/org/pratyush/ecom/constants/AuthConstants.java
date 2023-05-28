ea72cad60f9365a9c5431a860adb5eccdffc3580
Pratyush Choudhary <solixious@gmail.com>
2023-05-28 20:20:01 +0400

    Made health endpoint open so that access token is not needed when calling it.

--- src/main/java/org/pratyush/ecom/constants/AuthConstants.java
+++ src/main/java/org/pratyush/ecom/constants/AuthConstants.java
@@ -4,6 +4,7 @@ public interface AuthConstants {
     String ROLE = "role";
     String BEARER = "Bearer ";
     String[] OPEN_PATHS = new String[] {
+            UrlPath.HEALTH,
             UrlPath.USER + UrlPath.LOGIN,
             UrlPath.USER + UrlPath.TOKEN_GENERATE,
             UrlPath.USER + UrlPath.REGISTER
